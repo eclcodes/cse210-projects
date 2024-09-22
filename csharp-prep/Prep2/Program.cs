3d2f9e9b0c41c4858e9154adee4d6f9a3eeea8b3
Emma Loveless <georgetheawesomecat@gmail.com>
2024-09-22 00:04:13 -0600

    Completed core requirements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        float gradePercentage = float.Parse(percentage);
+        string letter = "";
+
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You got a {letter}.");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congrats! You've passed! All that hard work paid off!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass this time. Study more and try again!");
+        }
     }
 }
