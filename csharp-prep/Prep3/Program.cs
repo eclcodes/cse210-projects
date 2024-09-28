7359dea8d5ccea4bc4cd0183c5b624baf117b47b
Emma Loveless <georgetheawesomecat@gmail.com>
2024-09-27 18:57:53 -0600

    Completed core requirements

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain = "Y";
+
+        while (playAgain == "Y")
+        {
+
+            Random randomGenterator = new Random();
+            int magicNumber = randomGenterator.Next(1, 101);
+
+            int guess = 0;
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed correct!");
+                    Console.Write("Do you want to play again?(Y/N) ");
+                    playAgain = Console.ReadLine();
+                }
+            }    
+        }
     }
 }
